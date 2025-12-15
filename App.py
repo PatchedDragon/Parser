c3043a6e0fa205349e321beee5db75edfdf182c1
Ali Raza <alirazacoder1@gmail.com>
2025-12-15 21:52:10 +0500

    Fixed Parser Characters

--- App.py
+++ App.py
@@ -1,20 +1,18 @@
-
 from colorama import Fore, Style, init
 init(autoreset=True)
 
 def banner():
     logo = f"""{Fore.RED}
-    ██████╗  █████╗ ██████╗ ███████╗██████╗ 
-    ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
-    ██████╔╝███████║██████╔╝█████╗  ██████╔╝
-    ██╔═══╝ ██╔══██║██╔══██╗██╔══╝  ██╔══██╗
-    ██║     ██║  ██║██║  ██║███████╗██║  ██║
-    ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+    ██████   █████  ██████  ███████ ███████ ██████  
+    ██   ██ ██   ██ ██   ██ ██      ██      ██   ██ 
+    ██████  ███████ ██████  ███████ █████   ██████  
+    ██      ██   ██ ██   ██      ██ ██      ██   ██ 
+    ██      ██   ██ ██   ██ ███████ ███████ ██   ██ 
     """
 
     info = f"""
     {Fore.CYAN}Creator{Style.DIM}: ALI Raza
-    {Fore.CYAN}Github{Style.DIM}: https://github.com/PatchedDragon/Pasrser
+    {Fore.CYAN}Github{Style.DIM}: https://github.com/PatchedDragon/Parser
     {Fore.CYAN}Status{Style.DIM}: Under Development
     """
 
